42d9924119a4f9519a40feb1581fe8272da93f75
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-09-10 19:53:20 +0600

    [ADD] Music System Added

--- CoupleCromeExtension/audioLoader.js
+++ CoupleCromeExtension/audioLoader.js
@@ -1,3 +1,27 @@
+const AudioData = {
+  Audios: [
+    "audio/bewaja_bewaja.mp3",
+    "audio/Thoda Thoda.mp3",
+    "audio/Tumse Mohabbat Hai.mp3",
+    "audio/Tenu Leke.mp3",
+    "audio/Poran Jai Jolia Re.mp3",
+    "audio/Tooh.mp3",
+    "audio/Ranjha.mp3",
+    "audio/Chand Sifarish.mp3",
+    "audio/Hey Shona.mp3",
+    "audio/Falak Tak.mp3",
+    "audio/Maula Mere Maula.mp3",
+    "audio/Sarki Chunnariya Re Zara Zara.mp3",
+    "audio/Tu aake dekh le.mp3",
+    "audio/Paglu.mp3",
+    "audio/Mohombi.mp3",
+    "audio/Kya Baat Ay.mp3",
+    "audio/Copines.mp3",
+    "audio/Kiya Kiya.mp3",  
+  ],
+};
+
+
 var audio;
 
 document.querySelector("#play").addEventListener("click", function () {
@@ -15,8 +39,21 @@ document.querySelector("#stop").addEventListener("click", function () {
   }
 });
 
-function playAudio(text) {
-  text = "audio/bewaja_bewaja.mp3";
+
+
+
+
+
+
+function playAudio() {
+
+  const Audios = AudioData.Audios;
+  const randomIndex = Math.floor(Math.random() * Audios.length);
+  const randomAudio =Audios[randomIndex];
+  
+  console.log(randomAudio);
+
+  text = randomAudio;
   audio = new Audio(text);
   audio.play();
 }
